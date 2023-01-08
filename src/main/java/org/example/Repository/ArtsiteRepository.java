e445c689bf454817d314540915846d0fc5c9605d
Gabriel Charib <gabriel.charibpolls@helb-prigogine.be>
2023-01-08 23:50:30 +0100

    fix the bug with artist repository

--- src/main/java/org/example/Repository/ArtsiteRepository.java
+++ src/main/java/org/example/Repository/ArtsiteRepository.java
@@ -3,15 +3,13 @@ package org.example.Repository;
 
 import org.example.models.Artiste;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-//////////////
-//////impossible de rajouter ça
-
-/////////////////////
-
-//@Repository
-//public interface ArtsiteRepository extends JpaRepository<Artiste, Long> {
-//
 
 
-    //}
+@Repository
+public interface ArtsiteRepository extends CrudRepository<Artiste,Long> {
+
+
+    Artiste findArtistByName(String artisteName);
+}
--- src/main/java/org/example/controleur/ArtisteControleur.java
+++ src/main/java/org/example/controleur/ArtisteControleur.java
@@ -2,26 +2,47 @@ package org.example.controleur;
 
 
 
+import org.example.Repository.ArtsiteRepository;
 import org.example.models.Artiste;
 import org.example.services.ArtisteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/artistes")
-public class ArtisteControleur {
+//@RequestMapping(path = "/artistes")
+public class  ArtisteControleur {
 
     private final ArtisteService artisteService;
     //////////////////////////////////////
     //injection des dependances /////////
     /////////////////////////////////////
     @Autowired
+    private ArtsiteRepository artsiteRepository;
+
+    @Autowired
     public ArtisteControleur(ArtisteService artisteService) {
+
         this.artisteService = artisteService;
+    }
+
+
+
+    @PostMapping("/saveArtiste")
+    public String saveArtist(@RequestBody List<Artiste> artistesList){
+        artsiteRepository.saveAll(artistesList);
+        return artistesList.size()+"artiste enregistres";
+    }
+
+
+    @GetMapping("/getAll")
+    public List<Artiste> getAlArtiste(){
+        return (List<Artiste>) artsiteRepository.findAll();
+    }
+
+    public Artiste findUserByName(String ArtisteName){
+        return artsiteRepository.findArtistByName(ArtisteName);
     }
 
 
